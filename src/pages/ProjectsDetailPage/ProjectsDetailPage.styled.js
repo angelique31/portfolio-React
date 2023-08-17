f675cf5bdb83141cf38e6d9978bf11c2981245b0
angelique_rosin <angie1979.capucine@yahoo.fr>
2023-08-17 18:08:49 +0200

    Add vercel.json to fix 404 errors on direct page access

--- src/pages/ProjectsDetailPage/ProjectsDetailPage.styled.js
+++ src/pages/ProjectsDetailPage/ProjectsDetailPage.styled.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+export const SectionTitle = styled.h2`
+  font-size: 1.5rem;
+  margin: 15px 0;
+  color: white;
+`;
+
 export const SharedContainerStyles = `
   border: 1px solid rgba(255, 197, 118, 0.4);
   border-radius: 7px;
@@ -8,6 +14,10 @@ export const SharedContainerStyles = `
   background-color: rgb(49, 53, 82);
   &:hover {
     border-color: #ffc576;
+
+    ${SectionTitle} {
+      color: #ffc576;
+    }
   }
 `;
 
@@ -16,13 +26,6 @@ export const SharedTextStyles = `
   line-height: 1.5;
   color: rgb(209, 213, 219);
   margin-top: 20px;
-`;
-
-export const SectionTitle = styled.h2`
-  font-size: 1.5rem;
-  margin-top: 15px;
-  margin-bottom: 15px;
-  color: white;
 `;
 
 export const ProjectDetailContainer = styled.div`
@@ -35,41 +38,44 @@ export const IntroContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
   @media (max-width: 514px) {
     flex-direction: column;
   }
 `;
 
-export const ObjectiveContainer = styled.div`
+const Container = styled.div`
   ${SharedContainerStyles}
+  margin: 70px 0;
+  width: 100%;
+`;
 
+export const ObjectiveContainer = styled(Container)`
   width: 70%;
   margin: 70px auto;
-  &:hover {
-    ${SectionTitle} {
-      color: #ffc576;
-    }
-  }
   @media (max-width: 872px) {
     width: 100%;
-    margin: 0;
-    margin-top: 70px;
   }
 `;
 
-export const ChallengeContainer = styled.div`
-  ${SharedContainerStyles}
-
+export const ChallengeContainer = styled(Container)`
   width: 70%;
   margin: 70px auto;
-  &:hover {
-    ${SectionTitle} {
-      color: #ffc576;
-    }
-  }
+
   @media (max-width: 872px) {
-    width: 100%;
     margin: 80px 0;
+    width: 100%;
+  }
+`;
+
+export const ResponsiveContainer = styled(Container)`
+  width: 50%;
+
+  @media (max-width: 1060px) {
+    width: 70%;
+  }
+  @media (max-width: 514px) {
+    margin: 20px 0;
   }
 `;
 
@@ -77,58 +83,20 @@ export const AlignedImageContainer = styled.div`
   display: flex;
   gap: 60px;
   align-items: center;
+
   @media (max-width: 1060px) {
     flex-direction: column;
   }
 `;
 
-export const ResponsiveContainer = styled.div`
-  ${SharedContainerStyles}
-  width: 50%;
-
-  &:hover {
-    ${SectionTitle} {
-      color: #ffc576;
-    }
-  }
-  @media (max-width: 1060px) {
-    width: 70%;
-  }
-  @media (max-width: 872px) {
-    width: 100%;
-    margin: 80px 0;
-  }
-  @media (max-width: 514px) {
-    margin: 20px 0;
-  }
-`;
-
-export const AccessibilityContainer = styled.div`
-  ${SharedContainerStyles}
-  margin: 70px 0;
-
-  width: 100%;
-  &:hover {
-    ${SectionTitle} {
-      color: #ffc576;
-    }
-  }
-`;
-
-export const CollaborationContainer = styled.div`
-  ${SharedContainerStyles}
-  margin: 70px 0;
-  width: 100%;
-  &:hover {
-    ${SectionTitle} {
-      color: #ffc576;
-    }
-  }
+export const AccessibilityContainer = styled(Container)``;
+export const CollaborationContainer = styled(Container)``;
+export const TechnologyContainer = styled(Container)`
+  margin: 20px 0;
 `;
 
 export const MainTitle = styled.h1`
   font-size: 2rem;
-
   color: #ffb957;
 `;
 
@@ -143,34 +111,28 @@ export const StyledAnchorLink = styled.a`
 
   transition: box-shadow 0.2s;
   box-shadow: 0 0 10px rgba(255, 185, 87, 0.6);
+
   &:hover {
     background-color: #ffb957;
     color: #313552;
     box-shadow: 2px 1px 0px rgba(255, 185, 87, 0.8);
   }
+
   &:active {
     transform: scale(0.95);
     box-shadow: 0px 1px 7px rgba(255, 185, 87, 0.6);
   }
+
   @media (max-width: 514px) {
     margin: 20px;
   }
 `;
 
-export const TechnologyContainer = styled.div`
-  ${SharedContainerStyles}
-  margin: 20px 0;
-  width: 100%;
-  &:hover {
-    ${SectionTitle} {
-      color: #ffc576;
-    }
-  }
-`;
-
-export const TextParagraph = styled.p`
+const StyledText = styled.p`
   ${SharedTextStyles}
 `;
+
+export const TextParagraph = styled(StyledText)``;
 
 export const UnorderedList = styled.ul`
   ${SharedTextStyles}
@@ -179,6 +141,13 @@ export const UnorderedList = styled.ul`
 
 export const ListItem = styled.li`
   margin-bottom: 10px;
+`;
+
+const StyledImage = styled.img`
+  display: block;
+  border-radius: 7px;
+  margin-bottom: 20px;
+  overflow: hidden;
 `;
 
 export const ImageContainer = styled.div`
@@ -192,6 +161,7 @@ export const ImageContainer = styled.div`
 export const ImageWrapper = styled.div`
   width: 400px;
   height: 462px;
+
   @media (max-width: 872px) {
     margin-top: 50px;
   }
@@ -200,49 +170,36 @@ export const ImageWrapper = styled.div`
 export const ImageChallengeContainer = styled.div`
   display: flex;
   gap: 30px;
+
   @media (max-width: 532px) {
     flex-direction: column;
   }
 `;
 
-export const ProjectImage = styled.img`
+export const ProjectImage = styled(StyledImage)`
   width: 60%;
-
-  display: block;
-  border-radius: 7px;
-  margin-bottom: 20px;
   height: 200px;
-  overflow: hidden;
 
   @media (max-width: 872px) {
     margin: auto;
   }
+
   @media (max-width: 532px) {
     width: 100%;
   }
 `;
 
-export const AccessibilityImage = styled.img`
+export const AccessibilityImage = styled(StyledImage)`
   width: 100%;
   max-width: 300px;
-  display: block;
-  border-radius: 7px;
-  margin-bottom: 20px;
   max-height: 462px;
-  overflow: hidden;
-  margin: auto;
-  // @media (max-width: 872px) {
-  //   margin: auto;
-  // }
+  margin: 30px auto;
 `;
-export const MobileImage = styled.img`
+
+export const MobileImage = styled(StyledImage)`
   height: 300px;
   max-width: 300px;
-  display: block;
-  border-radius: 7px;
-  margin-bottom: 20px;
-  max-height: 462px;
-  overflow: hidden;
+
   @media (max-width: 872px) {
     margin: auto;
   }
