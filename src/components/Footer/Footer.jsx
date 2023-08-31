939bf751959e7dc8a4b4a40c48ea4267498a1e8f
angelique_rosin <angie1979.capucine@yahoo.fr>
2023-08-31 13:23:26 +0200

    add translate footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { useTranslation } from "react-i18next";
 import {
   FooterContainer,
   FooterParagraph,
@@ -12,34 +13,41 @@ import {
 import linkedin from "../../assets/logos/linkedin-icon.png";
 import github from "../../assets/logos/GitLogo.png";
 
-const Footer = () => (
-  <FooterContainer>
-    <FooterParagraph>
-      Conçu et développé par <OrangeText>Angélique Rosin</OrangeText>
-    </FooterParagraph>
-    <FooterParagraph>© 2023 - Angélique Rosin</FooterParagraph>
-    <div>
+const Footer = () => {
+  const { t } = useTranslation();
+  return (
+    <FooterContainer>
       <FooterParagraph>
-        <OrangeText>Retrouvez-moi sur :</OrangeText>
+        {t("footer.paragraphs.designedBy")}{" "}
+        <OrangeText>Angélique Rosin</OrangeText>
       </FooterParagraph>
-      <SocialList>
-        <SocialListItem>
-          <SocialLink href="https://www.linkedin.com/in/ang%C3%A9lique-rosin-5163a9231/">
-            <IconContainer>
-              <StyledImgLinkedin src={linkedin} alt="LinkedIn" />
-            </IconContainer>
-          </SocialLink>
-        </SocialListItem>
-        <SocialListItem>
-          <SocialLink href="https://github.com/angelique31?tab=repositories">
-            <IconContainer>
-              <StyledImgGit src={github} alt="Github" />
-            </IconContainer>
-          </SocialLink>
-        </SocialListItem>
-      </SocialList>
-    </div>
-  </FooterContainer>
-);
+
+      <FooterParagraph>{t("footer.paragraphs.copyright")}</FooterParagraph>
+
+      <div>
+        <FooterParagraph>
+          <OrangeText>{t("footer.paragraphs.findMe")}</OrangeText>
+        </FooterParagraph>
+
+        <SocialList>
+          <SocialListItem>
+            <SocialLink href="https://www.linkedin.com/in/ang%C3%A9lique-rosin-5163a9231/">
+              <IconContainer>
+                <StyledImgLinkedin src={linkedin} alt="LinkedIn" />
+              </IconContainer>
+            </SocialLink>
+          </SocialListItem>
+          <SocialListItem>
+            <SocialLink href="https://github.com/angelique31?tab=repositories">
+              <IconContainer>
+                <StyledImgGit src={github} alt="Github" />
+              </IconContainer>
+            </SocialLink>
+          </SocialListItem>
+        </SocialList>
+      </div>
+    </FooterContainer>
+  );
+};
 
 export default Footer;
--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,4 +1,3 @@
-// import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import projectsData from "../../datas/projectsData";
